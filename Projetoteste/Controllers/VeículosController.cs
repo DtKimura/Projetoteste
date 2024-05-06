52c634685d711877b544faefa90c1d43aa1fae18
DtKimura <dkimura@sga.pucminas.br>
2024-05-06 15:34:19 -0300

    detail delete

--- Projetoteste/Controllers/VeículosController.cs
+++ Projetoteste/Controllers/VeículosController.cs
@@ -56,6 +56,39 @@ namespace Projetoteste.Controllers
             }
             return View();
         }
-    }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var dados = await _context.Veículos.FindAsync(id);
+            if (dados == null)
+                return NotFound();
 
+            return View(dados);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var dados = await _context.Veículos.FindAsync(id);
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var dados = await _context.Veículos.FindAsync(id);
+            if (dados == null)
+                return NotFound();
+            _context.Veículos.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+
+        }
+    }
 }
